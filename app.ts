1e9f9efe1f8f60d4c1361b5fdba8f4fe5837cded
No Name <dev.haxuan@gmail.com>
2022-03-20 23:20:43 +0700

    base type

--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
-console.log('Understanding TypseScript...');
+/**
+ * Important: Type Casing
+ *  - It is string and number (etc), NOT String, Number etc.
+ *  - The core primitive types in JavaScript are all lowercase!
+*/
+
+function add(n1: number, n2: number, showResult: boolean, phrase: string){
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number'){
+    //     throw new Error('Incorrect input!')
+    // }
+    let result: number = n1 + n2;
+    if (showResult){
+        console.log(phrase + result);
+    } else{
+        return result;
+    }
+}
+
+console.log(add(10, 20, true, 'Result is: '));
