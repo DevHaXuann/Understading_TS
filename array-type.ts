aaf8cc20a1fbba09a79dae1e61c94248577ab0f4
Neil <dev.haxuan@gmail.com>
2023-02-25 22:51:37 +0700

    array type

---
+++ array-type.ts
@@ -0,0 +1,12 @@
+/**
+ * Có 2 cách để khai báo type cho 1 array
+ * 1. type[]
+ * 2. Array[type]
+ */
+
+// ví dụ:
+const numbers: number[] = [1, 3, 5];
+
+const nums: Array<number> = [1, 3, 5];
+
+
